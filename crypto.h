54bc52cc82108e917fb378bdd96656c667b8572b
inkgnt <120864571+inkgnt@users.noreply.github.com>
2025-01-28 09:58:54 +0300

    added functions for encryption/decryption using aes-256-cbc

--- crypto.h
+++ crypto.h
@@ -1,4 +1,9 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
+#include <QByteArray>
+
+QByteArray encryptAES256(const QByteArray& plaintext, const unsigned char* key, const unsigned char* iv);
+QByteArray decryptAES256(const QByteArray& ciphertext, const unsigned char* key, const unsigned char* iv);
+
 #endif // CRYPT_H
