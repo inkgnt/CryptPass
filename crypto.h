37d581c2fc33dd645b7ec98c4f87a732cd9995a4
inkogniito <vladi.shepard@gmail.com>
2025-04-05 22:03:02 +0300

    added functions for working with salt and hash, added logic for setting master password and logging into the application

--- crypto.h
+++ crypto.h
@@ -1,7 +1,25 @@
 #pragma once
 
 #include <vector>
+#include <QByteArray>
+#include <QString>
+#include <QFile>
+
+const int SALT_LENGTH = 16;
+const int HASH_LENGTH = 32;
+const int ITERATIONS = 10000;
+const QString HASH_FILE_PATH = "pHash.dat";
+
+inline bool isHashFileExists() {
+    QFile file(HASH_FILE_PATH);
+    return file.exists();
+}
 
 std::vector<unsigned char> encryptAES256(const std::vector<unsigned char>& plaintext, const unsigned char* key, const unsigned char* iv);
 std::vector<unsigned char> decryptAES256(const std::vector<unsigned char>& ciphertext, const unsigned char* key, const unsigned char* iv);
 
+bool loadHashAndSaltFromFile(std::vector<unsigned char> &storedSalt, std::vector<unsigned char> &storedHash);
+void saveHashAndSaltToFile(const std::vector<unsigned char> &Salt, const std::vector<unsigned char> &Hash);
+
+std::vector<unsigned char> generatePBKDF2Hash(const QString &password, const std::vector<unsigned char> &salt);
+std::vector<unsigned char> generateSalt();
--- registerwidget.cpp
+++ registerwidget.cpp
@@ -1,6 +1,9 @@
 #include "registerwidget.h"
 #include "ui_registerwidget.h"
 
+#include <QMessageBox>
+#include "crypto.h"
+
 RegisterWidget::RegisterWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::RegisterWidget)
@@ -12,3 +15,27 @@ RegisterWidget::~RegisterWidget()
 {
     delete ui;
 }
+
+void RegisterWidget::on_pushButton_clicked()
+{
+    QString password = ui->lineEdit->text();
+    QString confirmPassword = ui->lineEdit_2->text();
+
+    if (password.isEmpty() || confirmPassword.isEmpty()) {
+        QMessageBox::warning(this, "Error", "All fields must be filled in!");
+        return;
+    }
+
+    if (password != confirmPassword) {
+        QMessageBox::warning(this, "Error", "The passwords do not match!");
+        return;
+    }
+
+    auto salt = generateSalt();
+    auto hash = generatePBKDF2Hash(password, salt);
+
+    saveHashAndSaltToFile(salt, hash);
+
+    emit registerSuccess();
+}
+
